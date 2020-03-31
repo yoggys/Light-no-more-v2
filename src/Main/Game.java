21b5b0f5f76a57d85f60200dab22a652b906721b
yoggys <61660055+yoggys@users.noreply.github.com>
2020-03-31 13:54:04 +0200

    Update Game.java

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -9,7 +9,6 @@ public class Game {
 
 	public static void main(String[] args) 
 	{
-
 		JFrame window = new JFrame("Light no more");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
